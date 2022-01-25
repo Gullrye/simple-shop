9561b06d6a28ea4482d70cd0b49b8ff9f5b7ce64
Gullrye <wmwspecter@163.com>
2022-01-26 00:58:43 +0800

    build: config base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import styleImport, { VantResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     styleImport({
